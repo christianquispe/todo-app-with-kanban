6c49c4ba62ca1aae9ef9dc0472d90807f7b5de38
christianquispe <christianquispecamasca@gmail.com>
2023-07-09 02:58:33 -0500

    styles: format add delete unnecessary code

--- src/Tasks/InputCreateTask/InputCreateTask.tsx
+++ src/Tasks/InputCreateTask/InputCreateTask.tsx
@@ -1,4 +1,4 @@
-import   "./styles.css";
+import "./styles.css";
 
 // import { KeyboardEventHandler } from "react";
 import { useForm } from "react-hook-form";
@@ -13,19 +13,18 @@ export default function InputCreateTask() {
   const { register, handleSubmit } = useForm<InputCreateTaskInputs>();
   const { addTask } = useTasks();
 
-  // const handleKeyDown: KeyboardEventHandler = (e) => {
-  //   if (e.key === "Enter") {
-  //     addTask({ name });
-  //   }
-  // };
-
-  const submit = ( values : InputCreateTaskInputs) => {
-    addTask({name: values.name})
+  const submit = (values: InputCreateTaskInputs) => {
+    addTask({ name: values.name });
   };
 
   return (
     <form className="InputCreateTaskForm" onSubmit={handleSubmit(submit)}>
-      <input className="InputCreateTask" type="text" placeholder="Add new task..." {...register("name", { required: true })} />
+      <input
+        className="InputCreateTask"
+        type="text"
+        placeholder="Add new task..."
+        {...register("name", { required: true })}
+      />
     </form>
   );
 }
